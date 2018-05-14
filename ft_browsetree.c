04b853fb40465e07d283e60e675141e95157de8b
Jeremi RAYMOND <jraymond@e3r13p12.42.fr>
2018-05-14 04:28:14 +0200

    s

--- ft_browsetree.c
+++ ft_browsetree.c
@@ -6,15 +6,16 @@
 /*   By: jraymond <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/05/04 15:28:31 by jraymond          #+#    #+#             */
-/*   Updated: 2018/05/13 09:36:11 by jraymond         ###   ########.fr       */
+/*   Updated: 2018/05/14 04:11:43 by jraymond         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+#include "/Users/jraymond/ft_ls/ft_printf/ft_printf.h"
 
 void	ft_browsetree(t_btree *root, void (*dothings)(void *))
 {
-	if (!root)
+	if (!root || !dothings)
 		return ;
 	if (root->left)
 		ft_browsetree(root->left, dothings);
@@ -25,11 +26,11 @@ void	ft_browsetree(t_btree *root, void (*dothings)(void *))
 
 void	ft_revbrowsetree(t_btree *root, void (*dothings)(void *))
 {
-	if (!root)
+	if (!root || !dothings)
 		return ;
 	if (root->right)
-		ft_browsetree(root->right, dothings);
+		ft_revbrowsetree(root->right, dothings);
 	dothings(root->ptrdata);
 	if (root->left)
-		ft_browsetree(root->left, dothings);
+		ft_revbrowsetree(root->left, dothings);
 }
