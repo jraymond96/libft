e8b518d307547d652d814817f65caf7c0a2a2aff
Jeremi RAYMOND <jraymond@e3r4p19.42.fr>
2018-06-26 21:44:02 +0200

    s

--- ft_mem2join.c
+++ ft_mem2join.c
@@ -6,7 +6,7 @@
 /*   By: jraymond <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/06/25 18:46:40 by jraymond          #+#    #+#             */
-/*   Updated: 2018/06/25 18:53:28 by jraymond         ###   ########.fr       */
+/*   Updated: 2018/06/26 18:46:16 by jraymond         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,15 +14,17 @@
 
 void	ft_mem2join(char *dest, char *s1, char *s2, size_t size)
 {
-	if (size == 0 || !s1 || !s2)
-		return;
-	ft_strcpy(dest, s1);
+	if (s1)
+		ft_strcpy(dest, s1);
 	dest += ft_strlen(dest);
-	while (size-- && *s2)
+	if (s2)
 	{
-		*dest = *s2;
-		dest++;
-		s2++;
+		while (size-- && *s2)
+		{
+			*dest = *s2;
+			dest++;
+			s2++;
+		}
 	}
-	*s1 = '\0';
+	*dest = '\0';
 }
